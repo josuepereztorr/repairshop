feaaf45816ea8e2b9fc65b61dcd3e122952c8610
Josue <josuepereztorr@gmail.com>
2022-11-27 19:08:39 -0600

    - changed padding on drawer items

--- src/components/DrawerSelection.vue
+++ src/components/DrawerSelection.vue
@@ -1,31 +1,38 @@
 <template>
   <q-item
-      :to="path"
-      clickable
-      exact
-      active-class="text-accent"
+    :to="path"
+    clickable
+    exact
+    active-class="text-accent"
   >
-    <q-item-section v-if="icon" avatar>
-      <q-icon :name="icon" />
-    </q-item-section>
+    <div class="row q-ml-sm">
+      <q-item-section
+        v-if="icon"
+        avatar
+      >
+        <q-icon :name="icon" />
+      </q-item-section>
 
-    <q-item-section class="text-subtitle1">{{ title }}</q-item-section>
+      <q-item-section class="text-subtitle1">{{
+        title
+      }}</q-item-section>
+    </div>
   </q-item>
 </template>
 
 <script setup>
-  defineProps({
-    title: {
-      type: String,
-      required: true,
-    },
-    icon: {
-      type: String,
-      default: '',
-    },
-    path: {
-      type: String,
-      required: true,
-    }
-  })
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+    default: '',
+  },
+  path: {
+    type: String,
+    required: true,
+  },
+});
 </script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,70 +1,81 @@
- <template>
-    <q-layout view="lHr lpr lfr">
-      <q-drawer class="bg-primary text-white column items-start q-pa-md" v-model="leftDrawerOpen" side="left"  bordered>
-        <!-- Navigation -->
-        <q-list padding class="rounded-borders text-white full-width">
-          <!-- Brand -->
-          <q-item class="q-pb-sm">
-            <q-icon
-                size="md"
-                class="q-pr-sm"
-            >
-              <img src="@/assets/car-repair.png" alt="car logo">
-            </q-icon>
-            <q-item-section class="text-h6 text-weight-medium">Primary Auto Repair</q-item-section>
-          </q-item>
+<template>
+  <q-layout view="lHr lpr lfr">
+    <q-drawer
+      class="bg-primary text-white column items-start"
+      v-model="leftDrawerOpen"
+      side="left"
+      bordered
+    >
+      <!-- Navigation -->
+      <q-list
+        padding
+        class="rounded-borders text-white full-width"
+      >
+        <!-- Brand -->
+        <q-item class="q-pb-sm q-ml-sm">
+          <q-icon
+            size="md"
+            class="q-pr-sm"
+          >
+            <img
+              src="@/assets/car-repair.png"
+              alt="car logo"
+            />
+          </q-icon>
+          <q-item-section class="text-h6 text-weight-medium"
+            >Primary Auto Repair</q-item-section
+          >
+        </q-item>
 
-          <q-separator spaced />
+        <q-separator spaced />
 
-          <!-- Menu Selectors -->
-          <DrawerSelection
-              v-for="link in drawerMenuList"
-              :key="link.title"
-              v-bind="link"
-          ></DrawerSelection>
-        </q-list>
-      </q-drawer>
+        <!-- Menu Selectors -->
+        <DrawerSelection
+          v-for="link in drawerMenuList"
+          :key="link.title"
+          v-bind="link"
+          class=""
+        ></DrawerSelection>
+      </q-list>
+    </q-drawer>
 
-      <q-page-container>
-        <RouterView/>
-      </q-page-container>
+    <q-page-container>
+      <RouterView />
+    </q-page-container>
+  </q-layout>
+</template>
 
-    </q-layout>
-  </template>
+<script setup>
+import { ref } from 'vue';
+import DrawerSelection from '@/components/DrawerSelection';
 
-  <script setup>
-  import {ref} from 'vue';
-  import DrawerSelection from "@/components/DrawerSelection";
+// drawer
+const leftDrawerOpen = ref(true);
 
-  // drawer
-  const leftDrawerOpen = ref(true);
+// function toggleLeftDrawer () {
+//   leftDrawerOpen.value = !leftDrawerOpen.value
+// }
 
-  // function toggleLeftDrawer () {
-  //   leftDrawerOpen.value = !leftDrawerOpen.value
-  // }
-
-  const drawerMenuList = [
-    {
-      title: 'Customers',
-      icon: 'group',
-      path: '/customers',
-    },
-    {
-      title: 'Appointments',
-      icon: 'calendar_month',
-      path: '/appointments'
-    },
-    {
-      title: 'Repair Services',
-      icon: 'build',
-      path: 'repair-services',
-    },
-    {
-      title: 'Discounts',
-      icon: 'sell',
-      path: '/discounts',
-    }
-  ]
-  </script>
-
-
+const drawerMenuList = [
+  {
+    title: 'Customers',
+    icon: 'group',
+    path: '/customers',
+  },
+  {
+    title: 'Appointments',
+    icon: 'calendar_month',
+    path: '/appointments',
+  },
+  {
+    title: 'Repair Services',
+    icon: 'build',
+    path: 'repair-services',
+  },
+  {
+    title: 'Discounts',
+    icon: 'sell',
+    path: '/discounts',
+  },
+];
+</script>
