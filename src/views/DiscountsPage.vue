3102bb1c98e4957eff32ff414520c85680265353
Josue <josuepereztorr@gmail.com>
2022-12-02 17:43:07 -0600

    -moved css to global file

--- src/views/DiscountsPage.vue
+++ src/views/DiscountsPage.vue
@@ -200,17 +200,3 @@ const rows = [
   },
 ];
 </script>
-
-<style>
-/* Chrome, Safari, Edge, Opera */
-input::-webkit-outer-spin-button,
-input::-webkit-inner-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
-}
-
-/* Firefox */
-input[type='number'] {
-  -moz-appearance: textfield;
-}
-</style>
